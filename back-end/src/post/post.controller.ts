0ae4beda47260247762c85de5b69492935d6d989
Hingue-Mouko Yanick-Arnaud <yanick-arnaud.hingue-mouko@inetum.com>
2023-07-27 00:55:30 +0200

    #back-end: Ajout méthode de création d'un post

--- back-end/src/post/post.controller.ts
+++ back-end/src/post/post.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { AuthGuard } from '@nestjs/passport';
+import { CreatePostDto } from './dto/createPostDto';
+import { Request } from 'express';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
     constructor(private readonly postService: PostService) { }
 
-    @UseGuards(AuthGuard())
+    @UseGuards(AuthGuard('jwt'))
     @Post('create')
-    create() {
+    create(
+        @Body() createPostDto: CreatePostDto,
+        @Req() request: Request // Pour récupérer le user connecté
+    ) {
+        const userId = request.user['userId'];
+        return this.postService.create(createPostDto, userId);
     }
 }
--- back-end/src/post/post.service.ts
+++ back-end/src/post/post.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/createPostDto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        private readonly prismaService: PrismaService
+    ) { }
+    async create(createPostDto: CreatePostDto, userId: number) {
+        const { title, body } = createPostDto;
+        await this.prismaService.post.create({ data: { body, title, userId } });
+        return {data: 'post created'}
+    }
+}
