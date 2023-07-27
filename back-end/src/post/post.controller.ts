d0436e06c46b5ea4427614194bfbaaeb58ae317c
Hingue-Mouko Yanick-Arnaud <yanick-arnaud.hingue-mouko@inetum.com>
2023-07-27 02:06:16 +0200

    #back-end: Ajout méthode de suppression d'un post

--- back-end/src/post/post.controller.ts
+++ back-end/src/post/post.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CreatePostDto } from './dto/createPostDto';
 import { Request } from 'express';
+import { DeletePostDto } from './dto/deletePostDto';
 
 @Controller('posts')
 export class PostController {
     constructor(private readonly postService: PostService) { }
+
+    @Get()
+    getAll() {
+        return this.postService.getAll();
+    }
 
     @UseGuards(AuthGuard('jwt'))
     @Post('create')
@@ -14,7 +20,20 @@ export class PostController {
         @Body() createPostDto: CreatePostDto,
         @Req() request: Request // Pour récupérer le user connecté
     ) {
-        const userId = request.user['userId'];
+        const userId = request.user['userId']; // ** userId du user connecté
         return this.postService.create(createPostDto, userId);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete('delete/:id') // ** /:id permet d'avoir un id dynamique
+    delete(
+        // @Body() deletePostDto: DeletePostDto,
+        @Param('id', ParseIntPipe) postId: number, // ** @Param permet de récupérer les données depuis les parmètres de l'URL/ ParseIntPipe converti en INT
+        @Req() request: Request
+    ) {
+        const userId = request.user['userId'];
+        // return this.postService.delete(deletePostDto, userId);
+        return this.postService.delete(postId, userId);
+
     }
 }
--- back-end/src/post/post.service.ts
+++ back-end/src/post/post.service.ts
@@ -1,15 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreatePostDto } from './dto/createPostDto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { DeletePostDto } from './dto/deletePostDto';
 
 @Injectable()
 export class PostService {
     constructor(
         private readonly prismaService: PrismaService
     ) { }
+
+    async getAll() {
+        const posts = await this.prismaService.post.findMany({
+            include: { // ** Permet d'inclure les autres objets
+                // ** user: true ---> ici, il va inclure tout l'objet user avec toutes ses propriétés (username, email, password, etc...),
+                user: {
+                    select: { // ** select permet de spécifier les proprietés qu'on souhaite afficher
+                        username: true,
+                        email: true,
+                        password: false
+
+                    }
+                },
+                // ** comments: true
+                comments: {
+                    include: { // ** Permet d'inclure les autres objets 
+                        user: {
+                            select: { // ** select permet de spécifier les proprietés qu'on souhaite afficher
+                                username: true,
+                                email: true,
+                                password: false
+                            }
+                        }
+
+                    }
+                },
+            }
+        });
+        return posts;
+    }
     async create(createPostDto: CreatePostDto, userId: number) {
         const { title, body } = createPostDto;
         await this.prismaService.post.create({ data: { body, title, userId } });
-        return {data: 'post created'}
+        return { data: 'post created' }
+    }
+
+    async delete(postId: number, userId: number) {
+        // ** Vérifier que la publication existe
+        const post = await this.prismaService.post.findUnique({ where: { postId } });
+        if (!post) throw new NotFoundException('Post not found');
+
+        // ** Vérifier l'identité de celui qui essaie de supprimer le post
+        if (post.userId !== userId) throw new ForbiddenException('Forbidden action: Vous n\'êtes pas l\'auteur du post')
+
+        await this.prismaService.post.delete({ where: { postId, userId } });
+        return { data: 'post successfully deleted' }
+
     }
 }
